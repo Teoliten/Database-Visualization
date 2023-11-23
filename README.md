64aa0066def69aa40f093fff33d841c15c6c0e47
Teoliten <123169946+Teoliten@users.noreply.github.com>
2023-11-23 19:07:11 +0200

    Project start

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Database-Visualization
-
+This C++ program gets data from cloud, formats it and plots a graph for the data.
 ## Team
 - @Nicheen
 - @Teoliten
--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+vector<double> getData(vector<double>& data)
+{
+    cout << "Beep, boop, getting data from huge cloud..." << endl;
+    vector<double> cloud = {11.2, 25.2, 90.12, 99.22};
+    
+    data = cloud;
+    return data;
+}//getData
+
+void printData(const vector<double> data)
+{
+    cout << "Count: " << data.size() << endl;
+    cout << "Data: ";
+
+    for (int i = 0; i < data.size(); i++)
+    {
+        cout << data[i] << "%, ";
+    }
+    cout << endl;
+}//printData
+
+void plotData(const vector<double> data){
+    cout << "Cool graph:" << endl;
+}//plotData
+
 int main()
 {
-    
+    vector<double> data;
+
+    getData(data);
+    printData(data);
+    plotData(data);
+
     return 0;
 }//main
